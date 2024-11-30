6e5b32490ce27ec6ac3fbe6adaf1aaecef21ebd0
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2024-11-30 20:44:50 +0200

    change ui

--- lib/features/splash/presentation/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/widgets/splash_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:litlore/core/theme/colors.dart';
 import 'package:litlore/core/utils/assets.dart';
 
 class SplashViewBody extends StatelessWidget {
@@ -6,15 +8,36 @@ class SplashViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Image.asset(
-          AssetsData.logo,
-          
-        ),
-      ],
+    return Center(
+      child: Stack(
+        alignment: AlignmentDirectional.bottomCenter,
+        children: [
+          Image.asset(
+            AssetsData.logo,
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                "LitLoRe",
+                style: GoogleFonts.cinzelDecorative(
+                  color: kPrimaryColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 38,
+                ),
+              ),
+              Text(
+                "Library",
+                style: GoogleFonts.lustria(
+                  color: kPrimaryColor,
+                  fontSize: 28,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
