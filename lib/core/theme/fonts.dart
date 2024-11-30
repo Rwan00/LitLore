b2d3caa1c7395aca814d17eec6a3ac0d564a1e43
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2024-12-01 00:43:08 +0200

    add animation to splash screen

---
+++ lib/core/theme/fonts.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'colors.dart';
+
+TextStyle get splashStyle {
+  return GoogleFonts.cinzelDecorative(
+    color: kPrimaryColor,
+    fontWeight: FontWeight.bold,
+    fontSize: 26,
+  );
+}
+
+TextStyle get splashSubStyle {
+  return GoogleFonts.lustria(
+    color: kPrimaryColor,
+    fontSize: 22,
+  );
+}
--- lib/features/splash/presentation/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/widgets/splash_view_body.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:litlore/core/theme/colors.dart';
+
+import 'package:litlore/core/theme/fonts.dart';
 import 'package:litlore/core/utils/assets.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        seconds: 1,
+      ),
+    );
+    slideAnimation = Tween<Offset>(begin: const Offset(0, 4), end: Offset.zero)
+        .animate(animationController);
+
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,25 +44,27 @@ class SplashViewBody extends StatelessWidget {
         children: [
           Image.asset(
             AssetsData.logo,
+            width: 250,
+            fit: BoxFit.cover,
           ),
           Column(
             mainAxisSize: MainAxisSize.min,
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              Text(
-                "LitLoRe",
-                style: GoogleFonts.cinzelDecorative(
-                  color: kPrimaryColor,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 38,
-                ),
-              ),
+              AnimatedBuilder(
+                  animation: slideAnimation,
+                  builder: (context, _) {
+                    return SlideTransition(
+                      position: slideAnimation,
+                      child: Text(
+                        "LitLoRe",
+                        style: splashStyle,
+                      ),
+                    );
+                  }),
               Text(
                 "Library",
-                style: GoogleFonts.lustria(
-                  color: kPrimaryColor,
-                  fontSize: 28,
-                ),
+                style: splashSubStyle,
               ),
             ],
           ),
