3d172f10c2c6f1882389d097621500c3b307f428
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2024-12-07 20:26:05 +0200

    add home repo and failures class

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:litlore/core/errors/failures.dart';
+import 'package:litlore/features/home/data/models/book_model/book_model.dart';
+
+abstract class HomeRepo {
+ Future<Either<Failures,List<BookModel>>> fetchTopSellerBooks();
+  Future<Either<Failures,List<BookModel>>> fetchRecommendedBooks();
+}
