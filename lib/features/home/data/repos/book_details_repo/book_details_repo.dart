6afc838bfd626a7d30339563671e12c8c1ac1fa8
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2024-12-13 20:20:22 +0200

    fetch similar books

---
+++ lib/features/home/data/repos/book_details_repo/book_details_repo.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:litlore/core/errors/failures.dart';
+import 'package:litlore/features/home/data/models/book_model/book_model.dart';
+
+abstract class BookDetailsRepo {
+  Future<Either<Failures, List<BookModel>>> fetchSimilarBooks(
+      {required String category});
+}
---
+++ lib/features/home/data/repos/book_details_repo/book_details_repo_impl.dart
@@ -0,0 +1,43 @@
+import 'dart:developer';
+
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+import 'package:litlore/core/errors/failures.dart';
+
+import 'package:litlore/features/home/data/models/book_model/book_model.dart';
+
+
+import '../../../../../core/network/remote/api_service.dart';
+import 'book_details_repo.dart';
+
+class BookDetailsRepoImpl implements BookDetailsRepo {
+  final ApiService apiService;
+
+  BookDetailsRepoImpl({required this.apiService});
+
+  @override
+  Future<Either<Failures, List<BookModel>>> fetchSimilarBooks({required String category}) async{
+ 
+    try {
+      var data = await apiService.get(
+        endPoint: "volumes?q=$category+subject:&orderBy=relevance",
+      );
+      List<BookModel> books = [];
+      for (var item in data["items"]) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } on DioException catch (error) {
+      log(error.message ?? "");
+      return left(ServerFailure.fromDioError(error));
+    } catch (error) {
+      return left(
+        ServerFailure(
+          errorMsg: error.toString(),
+        ),
+      );
+    }
+  }
+}
+
